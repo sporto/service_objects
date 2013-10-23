7615387e655f4b435e97a9f348452f3237bdd9c6
Sebastian <sebasporto@gmail.com>
2013-10-24 10:15:52 +1100

    Ruby

---
+++ ruby/find_service.rb
@@ -0,0 +1,7 @@
+class FindService
+  
+  def run
+    [1,2]
+  end
+
+end
---
+++ ruby/process_service.rb
@@ -0,0 +1,23 @@
+require_relative "find_service"
+
+class ProcessService
+
+  attr_writer :find_service
+  
+  def run(input)
+    @input = input
+    complex_processing
+  end
+
+  def complex_processing
+     items = find_service.run
+     items.map do |i|
+        i * @input
+     end
+  end
+
+  def find_service
+    @find_service ||= FindService.new
+  end
+
+end
